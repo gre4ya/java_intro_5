b8e9fe63d0292382e173d1cf2565d30a7f818ab6
Andrii Grechyn <gre4ya@icloud.com>
2022-11-04 12:37:26 -0500

    added new lines of code

--- src/arrays/_15_CountDuplicates.java
+++ src/arrays/_15_CountDuplicates.java
@@ -1,30 +1,38 @@
 package arrays;
 
+import java.util.ArrayList;
+
 public class _15_CountDuplicates {
     public static void main(String[] args) {
 
-        int[] numbers = {2, 3 , 7 , 1, 1, 7, 1};
+        int[] numbers = {2, 3, 7, 1, 1, 7, 1, 3, 7, 7, 7, 9, 9};
         System.out.println(findHowManyDuplicates(numbers));
+        System.out.println(findHowManyDuplicates2(numbers));
     }
 
-    public static int findHowManyDuplicates(int[] numbers){
+    public static int findHowManyDuplicates(int[] numbers) {
 
         String dup = "";
 
         for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
-
-                // if we already checked it then break
-
-                if(dup.contains(numbers[i] + "")) break;
-
-                //if we didn't check it add it in dup container
-
-                if(numbers[i] == numbers[j]){
-                    dup += numbers[j] + ",";
-                }
+                if (!dup.contains(numbers[i] + "") && numbers[i] == numbers[j])
+                    dup += numbers[j] + "";
             }
         }
-        return dup.split(",").length;
+        return dup.length();
+    }
+
+    public static int findHowManyDuplicates2(int[] numbers) {
+
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (!nums.contains(numbers[i]) && numbers[i] == numbers[j])
+                    nums.add(numbers[j]);
+            }
+        }
+        return nums.size();
     }
 }
