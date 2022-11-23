c34c9aa9cadbdc70f3e947a2a7d61a29f99ca142
Andrii Grechyn <gre4ya@icloud.com>
2022-11-23 17:59:55 -0600

    added new lines of code

--- src/homeworks/Homework09.java
+++ src/homeworks/Homework09.java
@@ -2,6 +2,7 @@ package homeworks;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Homework09 {
     public static void main(String[] args) {
@@ -11,19 +12,31 @@ public class Homework09 {
         //int[] numbers = {-4, 0, -7, 0, 5, 10, 45, 45};
         //int[] numbers = {-8, 56, 7, 8, 65};
         int[] numbers = {3, 4, 3, 3, 5, 5, 6, 6, 7};
-        boolean isFound = false;
-        int firstDuplicate = 0;
 
+        ArrayList<Integer> numberList = new ArrayList<>();
+        for (Integer number : numbers) {
+            if(numberList.contains(number)) {
+                System.out.println(number);
+                break;
+            }
+                else numberList.add(number);
+
+        }
+
+
+        boolean isFound = false;
+        /*
         for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
                 if (numbers[i] == numbers[j] && !isFound) {
-                    firstDuplicate = numbers[i];
                     isFound = true;
+                    System.out.println(numbers[i]);
                     break;
                 }
             }
         }
-        System.out.println(isFound ? firstDuplicate : "There is no duplicates");
+        if(!isFound) System.out.println("There is no duplicates");
+         */
 
         System.out.println("-----------Task-2-----------");
 
@@ -46,31 +59,37 @@ public class Homework09 {
 
         System.out.println("-----------Task-3-----------");
 
-        //int[] newNumbers = {0, -4, -7, 0, 5, 10, 45, -7, 0};
-        int[] newNumbers = {1, 2, 5, 0, 7};
+        int[] newNumbers = {0, -4, -7, 0, 5, 10, 45, -7, 0};
+        //int[] newNumbers = {1, 2, 5, 0, 7};
         ArrayList<Integer> nums = new ArrayList<Integer>();
 
         for (int i = 0; i < newNumbers.length - 1; i++) {
             for (int j = i + 1; j < newNumbers.length; j++) {
                 if (newNumbers[i] == newNumbers[j] && !nums.contains(newNumbers[i])) {
                     nums.add(newNumbers[i]);
+                    System.out.println(newNumbers[i]);
                 }
             }
         }
 
         if(nums.isEmpty()) System.out.println("There is no duplicates");
-        else nums.forEach(System.out::println);
+       // else nums.forEach(System.out::println);
 
         System.out.println("-----------Task-4-----------");
 
         String[] newWords = {"A", "foo", "12", "Foo", "bar", "a", "a", "java"};
         //String[] newWords = {"python", "foo", "bar", "java", "123"};
+
         String newDuplicate = "";
+
         for (int i = 0; i < newWords.length - 1; i++) {
             for (int j = i + 1; j < newWords.length; j++) {
-                if (newWords[i].equalsIgnoreCase(newWords[j]) && !newDuplicate.contains(newWords[i].toLowerCase())) {
+                if (newWords[i].equalsIgnoreCase(newWords[j]) &&
+                        !newDuplicate.contains(newWords[i].toLowerCase())) {
+
                     newDuplicate += newWords[i].toLowerCase();
                     System.out.println(newWords[i]);
+
                 }
             }
         }
@@ -79,24 +98,42 @@ public class Homework09 {
         System.out.println("-----------Task-5-----------");
 
         //String[] words1 = {"abc", "foo", "bar"};
-        String[] words1 = {"java", "python", "ruby"};
-        //String[] words1 = {"java", "python", "ruby", "cat", "' '", "apple"};
+        //String[] words1 = {"java", "python", "ruby"};
+        String[] words1 = {"java", "python", "ruby", "cat", "' '", "apple", "A"};
+        int firstIndex = 0;
+        int lastIndex = words1.length - 1;
+        String storage;
+        while(firstIndex < lastIndex){
+            storage = words1[firstIndex];
+            words1[firstIndex] = words1[lastIndex];
+            words1[lastIndex] = storage;
+            firstIndex++;
+            lastIndex--;
+        }
+        System.out.println(Arrays.toString(words1));
+
+        /*
+        Collections.reverse(Arrays.asList(words1));
+        System.out.println(Arrays.toString(words1));
+
+        System.out.println("----------------------------");
 
         String[] reverse = new String[words1.length];
         for (int i = words1.length - 1; i >= 0; i--) {
             reverse[words1.length - 1 - i] = words1[i];
         }
         System.out.println(Arrays.toString(reverse));
+        */
 
         System.out.println("-----------Task-6-----------");
 
         //String str = "Java is fun";
         String str = "Today is a fun day";
-        String result = "";
-        String[] str1 = str.split(" ");
+        StringBuilder result = new StringBuilder();
+        String[] str1 = str.split("[ ]+");
         for (String s : str1) {
-            result += new StringBuilder(s).reverse() + " ";
+            result.append(new StringBuilder(s).reverse()).append(" ");
         }
-        System.out.println(result.substring(0, result.length()-1));
+        System.out.println(result.toString().trim());
     }
 }
