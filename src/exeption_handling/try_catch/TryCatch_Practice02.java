4816b969286da40d493ae3d04c5d562b908d5fa7
Andrii Grechyn <gre4ya@icloud.com>
2022-12-10 11:38:54 -0600

    added new lines of code

--- src/exeption_handling/try_catch/TryCatch_Practice02.java
+++ src/exeption_handling/try_catch/TryCatch_Practice02.java
@@ -2,17 +2,27 @@ package exeption_handling.try_catch;
 
 import utilities.ScannerHelper;
 
+import java.util.InputMismatchException;
+
 public class TryCatch_Practice02 {
     public static void main(String[] args) {
 
-        int num1 = ScannerHelper.getANumber();
-        int num2 = ScannerHelper.getANumber();
         try {
+            int num1 = ScannerHelper.getANumber();
+            int num2 = ScannerHelper.getANumber();
             System.out.println(num1 / num2);
         }
-        catch (ArithmeticException e){
+        catch (InputMismatchException | ArithmeticException e){
             e.printStackTrace();
         }
+//    combine catch:
+//        catch (InputMismatchException e){
+//            e.printStackTrace();
+//        }
+//        catch (ArithmeticException e){
+//            e.printStackTrace();
+//        }
+
         System.out.println("End of the program");
 
     }
