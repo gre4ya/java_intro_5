f3a8c9bb972833d07cdc426282f4016bd843c70b
Andrii Grechyn <gre4ya@icloud.com>
2022-12-10 14:59:34 -0600

    added new lines of code

--- src/exeption_handling/custom_exeptions/TestPermission.java
+++ src/exeption_handling/custom_exeptions/TestPermission.java
@@ -2,20 +2,31 @@ package exeption_handling.custom_exeptions;
 
 import utilities.ScannerHelper;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TestPermission {
     public static void main(String[] args) {
 
         int age = ScannerHelper.getAnAge();
-        try{
-            if (Permission.isAgeValid(age)) System.out.println("You can get DL");
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+        try {if (Permission.isAgeValid(age)) System.out.println("You can get DL");}
+        catch (Exception e) {e.printStackTrace();}
+        finally {System.out.println("you are " + age + ".");}
+
+        /*
+        Write a program to ask user to enter the day
+        Let user know the check in hours
+        In case of any error, catch it and print the error
+        Finally, print the current date - day
+         */
+
+        int day = ScannerHelper.getANumber();
+        try{System.out.println(Permission.getCheckInHours(day));}
+        catch (Exception e) {e.printStackTrace();}
         finally {
-            System.out.println("you are " + age + ".");
+            Date date = new Date(); // Sat Dec 10 14:53:59 CST 2022
+            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE"); // Saturday
+            System.out.println("The current date is " + dayFormat.format(date));
         }
     }
-
-
 }
