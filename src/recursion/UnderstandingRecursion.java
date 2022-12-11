5a20a32b61b265710b61a169070e384c6e937e27
Andrii Grechyn <gre4ya@icloud.com>
2022-12-11 14:46:50 -0600

    added new lines of code

---
+++ src/recursion/UnderstandingRecursion.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class UnderstandingRecursion {
+
+    public static void print(){
+        System.out.println("Hello!");
+        print(); // StackOverflowError
+    }
+    public static void printNumbersRecursively(int num){
+        if(num == 0) return;
+        printNumbersRecursively(num - 1);
+        System.out.println(num);
+
+    }
+
+
+
+    public static void main(String[] args) {
+       //  print();  // StackOverflowError
+
+    printNumbersRecursively(6);
+
+
+    }
+}
