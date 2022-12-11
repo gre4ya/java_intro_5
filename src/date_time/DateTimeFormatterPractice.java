1d5c882bcd74efc4e73feec4f12bd5e4df0d913d
Andrii Grechyn <gre4ya@icloud.com>
2022-12-11 13:29:45 -0600

    added new lines of code

---
+++ src/date_time/DateTimeFormatterPractice.java
@@ -0,0 +1,16 @@
+package date_time;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatterPractice {
+    public static void main(String[] args) {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime); // 2022-12-11T13:24:25.658
+
+        System.out.println(localDateTime.format(DateTimeFormatter.ofPattern("MM/dd/yyyy"))); // 12/11/2022
+        System.out.println(localDateTime.format(DateTimeFormatter.ofPattern("E hh:mm a"))); // Sun 01:27 PM
+    }
+
+}
