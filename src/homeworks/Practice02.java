48536712397eb01c4d771d377a24054daf67ab4b
Andrii Grechyn <gre4ya@icloud.com>
2022-12-13 10:40:51 -0600

    added new lines of code

--- src/homeworks/Practice02.java
+++ src/homeworks/Practice02.java
@@ -7,30 +7,27 @@ public class Practice02 {
         int i = 0;
 
         while(i < str.length() - 2){
-            if(str.charAt(i) == 'z' && str.charAt(i + 2) == 'p'){
-
-                newStr += str.substring(0, i + 1);
-                str = str.substring(i + 2);
-                i = 0;
-            }
-            i++;
+          if(str.charAt(i) == 'z' && str.charAt(i + 2) == 'p'){
+            newStr += str.substring(0, i + 1);
+            str = str.substring(i + 2);
+            i = 0;
+          }
+          i++;
         }
         newStr += str;
         System.out.println(newStr);
-
 
         str = "abbCCCddBBBxx";
         int index = 0, freq = 0, count = 0;
         char[] arr = str.toCharArray();
         for (int o = 0, j = o + 1; o < arr.length - 1; o++, j++) {
-            if(arr[j] == arr[o]) count++;
-            if(count >= freq) {
-                index = o;
-                freq = count;
-                count = 0;
-            }
+          if(arr[j] == arr[o]) count++;
+          if(count >= freq) {
+            index = o;
+            freq = count;
+            count = 0;
+          }
         }
         System.out.println(arr[index]);
-
     }
 }
