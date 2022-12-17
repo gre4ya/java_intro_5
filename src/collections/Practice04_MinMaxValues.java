5f0ec3b9585c587895bc8cd30342fc988cadb7ca
Andrii Grechyn <gre4ya@icloud.com>
2022-12-17 14:57:24 -0600

    added new lines of code

--- src/collections/Practice04_MinMaxValues.java
+++ src/collections/Practice04_MinMaxValues.java
@@ -1,6 +1,8 @@
 package collections;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TreeSet;
 
 public class Practice04_MinMaxValues {
@@ -27,6 +29,19 @@ public class Practice04_MinMaxValues {
         System.out.println(secondMax(new Integer[]{12})); // null
         System.out.println(secondMax(new Integer[]{})); // null
 
+        System.out.println(secondMin(new Integer[]{10, 100, 123, 507, 25})); // 25
+        System.out.println(secondMin(new Integer[]{5, 5, 5, 5})); // null
+        System.out.println(secondMin(new Integer[]{12})); // null
+        System.out.println(secondMin(new Integer[]{})); // null
+
+        System.out.println("\n-----------------------------\n");
+
+        System.out.println(nthMax(new ArrayList<>(Arrays.asList(10, 100, 123, 507, 25)), 3)); // 100
+        System.out.println(nthMin(new ArrayList<>(Arrays.asList(10, 101, 123, 507, 25)), 3)); // 101
+        System.out.println(nthMax(new ArrayList<>(Arrays.asList(100, 100, 100, 100, 100)), 3)); // null
+        System.out.println(nthMax(new ArrayList<>(Arrays.asList()), 3)); // null
+        System.out.println(nthMax(new ArrayList<>(Arrays.asList(23, 25)), 3)); // null
+
 
     }
 
@@ -48,6 +63,41 @@ public class Practice04_MinMaxValues {
         }
         else return null;
     }
+    public static Integer secondMin(Integer[] arr) {
+        if (arr.length > 1) {
+            try {
+                TreeSet<Integer> arr1 = new TreeSet<>(Arrays.asList(arr));
+                arr1.pollFirst();
+                return arr1.first();
+            }catch (Exception e){
+                return null;
+            }
+        }
+        else return null;
+    }
+
+    public static Integer nthMax(List<Integer> numbers, int nth){
+        TreeSet<Integer> set = new TreeSet<>(numbers);
+        if(set.size() < nth) return null;
+
+        //nth-1 time pollLast() and then return last()
+        for (int i = 1; i <= nth-1; i++) {
+            set.pollLast();
+        }
+        return set.last();
+    }
+    public static Integer nthMin(List<Integer> numbers, int nth){
+        TreeSet<Integer> set = new TreeSet<>(numbers);
+        if(set.size() < nth) return null;
+
+        //nth-1 time pollFirst() and then return first()
+        for (int i = 1; i <= nth-1; i++) {
+            set.pollFirst();
+        }
+        return set.first();
+    }
+
+
 
 
 }
