7839bccf796bf89ad1d026610b0c59fb47f2d32a
Andrii Grechyn <gre4ya@icloud.com>
2022-12-18 14:58:26 -0600

    added new lines of code

--- src/collections/Practice07_ProductPrice.java
+++ src/collections/Practice07_ProductPrice.java
@@ -6,17 +6,20 @@ import java.util.TreeSet;
 
 public class Practice07_ProductPrice {
     public static void main(String[] args) {
-        HashMap<String, Integer> products = new HashMap<>();
-        products.put("iPhone", 1000);
-        products.put("Macbook Pro", 1300);
-        products.put("iMac", 1500);
-        products.put("AirPods", 200);
-        products.put("iPad", 700);
+        HashMap<String, Double> products = new HashMap<>();
+        products.put("iPhone", 1000.0);
+        products.put("Macbook Pro", 1300.0);
+        products.put("iMac", 1500.0);
+        products.put("AirPods", 200.0);
+        products.put("iPad", 700.0);
 
-        int maxPrice = new TreeSet<>(products.values()).last();
-        for (Map.Entry<String, Integer> entry : products.entrySet()) {
-            if(entry.getValue() == maxPrice) System.out.println(entry.getKey() +
-                    " is the most expencive with the price of $" + entry.getValue());
+        double maxPrice = new TreeSet<>(products.values()).last();
+        for (Map.Entry<String, Double> entry : products.entrySet()) {
+            if (entry.getValue() == maxPrice) {
+                System.out.println(entry.getKey() +
+                        " is the most expencive with the price of $" + entry.getValue());
+                break;
+            }
         }
     }
 }
