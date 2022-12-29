5c0410c24cf17aecf10bcfbda25fc68e7fd4c033
Andrii Grechyn <gre4ya@icloud.com>
2022-12-29 11:10:54 -0600

    added new lines of code

--- src/homeworks/Homework13.java
+++ src/homeworks/Homework13.java
@@ -29,9 +29,25 @@ public class Homework13 {
         System.out.println("---------------Task-4---------------");
 
         System.out.println(containsValue(new String[]{"abc", "foo", "java"}, "hello"));
+        System.out.println(containsValue(new String[]{"abc", "def", "123"}, "Abc"));
+        System.out.println(containsValue(new String[]{"abc", "def", "123", "Java", "Hello"}, "123"));
+
+        System.out.println("---------------Task-5---------------");
+
+        System.out.println(reverseSentence("Hello"));
+        System.out.println(reverseSentence("Java is fun"));
+        System.out.println(reverseSentence("This is a sentence"));
+
+        System.out.println("---------------Task-6---------------");
+
+        System.out.println(removeStringSpecialsDigits("123Java #$%is fun"));
+        System.out.println(removeStringSpecialsDigits("Selenium"));
+        System.out.println(removeStringSpecialsDigits("Selenium 123#$%Cypress"));
+
+        System.out.println("---------------Task-7---------------");
+
 
     }
-
     ///////////////////////Task-1///////////////////////
     public static boolean hasLowerCase(String str){
         boolean result = false;
@@ -69,5 +85,28 @@ public class Homework13 {
         return result;
     }
     ///////////////////////Task-5///////////////////////
+    public static String reverseSentence(String str){
+        str = str.toLowerCase().trim();
+        String result = "";
+        if(!str.contains(" ")) return "There is not enough words!";
+        else{
+            String[] arr = str.split(" ");
+            for (int i = arr.length - 1; i >= 0; i--) {
+                result += arr[i] + " ";
+            }
+        }
+        result = result.substring(0,1).toUpperCase().concat(result.substring(1,result.length() - 1));
+        return result;
+    }
+    ///////////////////////Task-6///////////////////////
+    public static String removeStringSpecialsDigits(String str){
+        String result = "";
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if(Character.isLetter(arr[i]) || Character.isWhitespace(arr[i]))
+                result += arr[i];
+        }
+        return result;
+    }
 
 }
